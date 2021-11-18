9ceeb1026966f59a88f56b5fc235c77f92856a8a
carlop3333 <77977040+carlop3333@users.noreply.github.com>
2021-11-17 22:02:21 -0400

    forgot about tags and jsdoc

--- src/tmh.ts
+++ src/tmh.ts
@@ -11,11 +11,15 @@ export class gameData {
 		let scoreboard = Commands.run(`scoreboard players list "${PlayerName}"`,World.getDimension("overworld"));
     }
 
+	/**
+	 * 
+	 * @param PlayerName player name
+	 * @param Tags  tags that the player has
+	 * @returns true (or false if tag doesnt exists ) 
+	 */
 	hasTags(PlayerName: string, Tags:string) {
 		let tagslist = Commands.run(`tags "${PlayerName}" list`,World.getDimension("overworld"));
-		if (tagslist.search(Tags)) {
-
-		}
+		if (tagslist.search(Tags)) { return true; } else {throw false;}
 	}
 
 }	
