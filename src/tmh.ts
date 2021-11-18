0140a1ba12e2272ea05193d2ee932d6317f7dc85
carlop3333 <77977040+carlop3333@users.noreply.github.com>
2021-11-17 20:58:26 -0400

    (staging) added hasTag func

--- src/tmh.ts
+++ src/tmh.ts
@@ -2,12 +2,20 @@
  *
  */
 
-import {Commnands, World} from "mojang-minecraft";
+import {Commands, World} from "mojang-minecraft";
 
-export class ScoreboardData {
+export class gameData {
 
 
 	getScoreboardNumber(ObjectiveName: string, PlayerName: string) {
-		let scoreboard = Commnands.run(`scoreboard players list "${PlayerName}"`,World.getDimension("overworld"))
+		let scoreboard = Commands.run(`scoreboard players list "${PlayerName}"`,World.getDimension("overworld"));
     }
-}
+
+	hasTags(PlayerName: string, Tags:string) {
+		let tagslist = Commands.run(`tags "${PlayerName}" list`,World.getDimension("overworld"));
+		if (tagslist.search(Tags)) {
+
+		}
+	}
+
+}	
